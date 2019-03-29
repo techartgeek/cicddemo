bbc8bd444ff1f5784e74df4ce38b277e8fb3bb7f
Vikas Sharma <vikasharma150880@gmail.com>
2019-03-29 12:22:24 +0000

    updated message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send("Hello World Peace! Are you there?"));
+app.get('/', (req, res) => res.send("Hello World!"));
 
 app.get('*', (req, res) => res.redirect('/'));
 
